176cbfbbfcff4b39b84e2595fe854314a39455c8
DoniyorbekAbdugopporov <doniyorbekabdugopporov40@gmail.com>
2024-11-28 22:57:11 +0500

    fixed carusel and dark mode

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Modal = ({ imgUrl, setOpen }) => {
   return (
-    <div className="w-full h-full fixed top-0 left-0 bg-[#0008] flex items-center justify-center">
-      <div className="bg-white pt-16 px-8 pb-8 rounded-lg max-w-[500px] relative">
+    <div className="w-full h-full fixed top-0 left-0 bg-[#000A] flex items-center justify-center z-10 ">
+      <div className="bg-white pt-16 px-8 pb-8 rounded-lg max-w-[500px] relative dark:bg-slate-300">
         <span
           onClick={() => setOpen(false)}
           className="absolute top-4 right-6 text-xl cursor-pointer"
